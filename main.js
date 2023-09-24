6dba3edd29f21c4540b3d80d85f1552dd9a5ef0e
Mani1904 <144296757+Mani1904@users.noreply.github.com>
2023-09-23 21:44:46 -0700

    Add files via upload
    drunkers

---
+++ main.js
@@ -0,0 +1,21 @@
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () =>
+{
+search.classList.toggle('active');
+navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () =>
+{
+search.classList.remove('active');
+navbar.classList.toggle('active');
+}
+ 
+
+
+let header= document.querySelector('header');
+window.addEventListener('scroll' , () => 
+{
+header.classList.toggle('shadow', window.scrollY > 0);
+});
